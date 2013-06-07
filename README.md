5b9b35ec3ceecf19722cc5b313994377475c57da
Eddie Lee <eddieberklee@gmail.com>
2013-06-07 01:54:10 -0700

    init

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-rails-unobtrusive-login
-=======================
 
-Playing around with how login is handled in Rails.
+https://www.google.com/search?q=rails+3+authlogic+tutorial&oq=rails+3+authlogic+tutorial&aqs=chrome.0.57j0l2j62.3723j0&sourceid=chrome&ie=UTF-8
+http://saravani.wordpress.com/2012/01/03/rails-3-authlogic-implementation-a-basic-tutorial/
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnobtrusiveLogin::Application.config.secret_token = '065b37d60056bcabaf0d7c602c9d82c08200ed0637ff406abc8d00222f35ff971ca275fdc0df31b99c73dc0ca02893289ac694734d15cdd424078c638d96adb4'
